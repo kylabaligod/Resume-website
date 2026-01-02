2707dbde30441ad6f3beab3969ed11cc5ee23c41
Kyla Baligod <kylabaligod@gmail.com>
2026-01-02 17:48:25 +0800

    Update resume site

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-
-var welcome_message = "This is a welcome message stored inside a variable";
+/* var welcome_message = "This is a welcome message stored inside a variable";
 
 alert(welcome_message);
 
@@ -11,7 +10,8 @@ if(isPremiumUser == true) {
     console.log("Thanks for being a loyal customer")
 }else{
     console.log("You should subscribe to our premium service.")
-}
+} 
+
 
 var btn = document.getElementById("go-button");
 
@@ -24,4 +24,21 @@ btn.addEventListener("click", buttonClicked);
 
 var numbers = [43, 55, 10, 25, 27, 33, 101, 102, 603, 707, 99, 87]
 
-var numero = [2, 5, 3, 6, 1, 9, 4]
+var numero = [2, 5, 3, 6, 1, 9, 4] */
+
+/*@media (max-width: 768px) {
+.skills-grid {
+    grid-template-columns: PaymentRequestUpdateEvent(2 1fr);
+    }
+}
+
+@media (max-width: 480px){
+    .skills-grid {
+        grid-template-columns: 1fr;
+    }
+}*/
+
+"use strict";
+
+const x = 23;
+if (x === 23) console.log(23);
